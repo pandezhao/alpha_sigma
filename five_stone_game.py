3445c83bb42043b89b5fd6ed6be6f5e4ca97ebd7
pandezhao <981317572@qq.com>
2019-02-16 15:58:25 -0500

    writing the simulation now

--- five_stone_game.py
+++ five_stone_game.py
@@ -5,33 +5,15 @@ class main_process:
     def __init__(self, board_size=15, AI = None):
         self.board_size = board_size
         self.board = np.zeros([self.board_size+8, self.board_size+8])
-        self.valid_backup = []
         self.current_player = 1
         self.passed = 0
         if AI:
             self.AI = "random"
 
-        self.games_counter = 0
-        self.games_black_win_counter = 0
-        self.games_white_win_counter = 0
-        self.no_win_games_counter = 0
-
-        self.tmp_black_win = 0
-        self.tmp_white_win = 0
-        self.tmp_heqi = 0
-
-        self.board_record = []
-        self.place_record = []
-
     def renew(self):
         self.board = np.zeros([self.board_size+8, self.board_size+8])
         self.current_player = 1
         self.passed = 0
-
-    def clear_tmp(self):
-        self.tmp_black_win = 0
-        self.tmp_white_win = 0
-        self.tmp_heqi = 0
 
     def vs_AI(self):
         pass
@@ -39,30 +21,46 @@ class main_process:
     def which_player(self):
         return self.current_player
 
+    def current_board_state(self):
+        return self.board
+
+    def simulate_reset(self, board_state):  # 这里还可以添加一个检测输入棋盘是否为一局胜负以分的board的算法，但是由于不影响强化学习，所以暂且搁置
+        if type(board_state) != np.ndarray: # 而且还有一部分原因也是因为没有找到一个合适的算法。
+            raise ValueError("board_state must be a np array")
+        if board_state.shape[0]!=self.board_size or board_state.shape[1]!=self.board_size:
+            raise ValueError("board size is different from the given size")
+        self.board = board_state
+        step_count, black_count, white_count = 0, 0, 0
+        for hang in board_state:
+            for lie in hang:
+                if lie == 1:
+                    step_count += 1
+                    black_count += 1
+                elif lie == -1:
+                    step_count += 1
+                    white_count += 1
+                elif lie != 0:
+                    raise ValueError("We got some value wrong in the input board_state, the value in board must be 0, 1, -1")
+        if black_count == white_count:
+            self.current_player = 1
+            self.passed = step_count
+        elif black_count == white_count + 1:
+            self.current_player = -1
+            self.passed = step_count
+        else:
+            raise ValueError("The input board state is not a standard five stone game board, the number of black stone"
+                             " and white stone is incorrect")
+
     def step(self, place):
         self.passed += 1
-        self.place_record.append(place)
         if not self.board[place[0]+4, place[1]+4]:
             self.board[place[0]+4, place[1]+4] = self.current_player
             self.current_player = -self.current_player
             self.last_step = [place[0]+4, place[1]+4]
             if self.check_win():
-                self.games_counter += 1
-                if self.current_player == 1:
-                    self.games_black_win_counter += 1
-                    self.tmp_black_win += 1
-                else:
-                    self.games_white_win_counter += 1
-                    self.tmp_white_win += 1
-
                 return False, self.board[4:self.board_size+4, 4:self.board_size+4]
             elif self.passed == self.board_size * self.board_size:
-                self.games_counter += 1
-                self.no_win_games_counter += 1
-                self.tmp_heqi += 1
-
                 return None, self.board[4:self.board_size+4, 4:self.board_size+4]
-
             return True, self.board[4:self.board_size+4, 4:self.board_size+4]
         else:
             raise ValueError("here already has a stone, you can't please stone on it")
--- new_MCTS.py
+++ new_MCTS.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random
 import utils
-from five_stone_game import main_process
+from five_stone_game import main_process as five_stone_game
 
 num2char = {0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h", 8: "i", 9: "j", 10: "k", 11: "l", 12: "m",
             13: "n", 14: "o", 15: "p", 16: "q", 17: "r", 18: "s", 19: "t", 20: "u"}
@@ -31,23 +31,28 @@ class edge:
             return node(new_state_name, self, 1 - self.parent_node.node_player)
 
 class node:
-    def __init__(self, state, parent, player):
+    def __init__(self, state, parent, player, MCTS_pointer):
         self.state_name = state
         self.parent = parent
         self.value = 0
         self.counter = 0
         self.child = {}
+        self.MCTS_pointer = MCTS_pointer
 
         self.node_player = player
 
     def get_state(self):
         return self.state_name
 
-    def add_child(self, state, action, priorP):
-        self.child[action] = edge(action=action, parent_node=self, priorP=priorP)
+    def add_child(self, action, priorP):
+        action_name = utils.move_to_str(action)
+        self.child[action_name] = edge(action=action, parent_node=self, priorP=priorP, MCTS_pointer=self.MCTS_pointer)
 
     def get_child(self, action):
         return self.child[action].get_child()
+
+    def eval_or_not(self):
+        return len(self.child)==0
 
     def backup(self, v):
         self.value += v
@@ -69,20 +74,24 @@ class MCTS:
         self.database = [[node(init_state, init_node)]]
         self.current_node = self.database[0][0]
         self.NN = neural_network
-        self.game_process = main_process(board_size=board_size)
+        self.game_process = five_stone_game(board_size=board_size)
+        self.simulate_game = five_stone_game(board_size=board_size)
 
     def search_node(self, node_name):
 
 
-    def simulation(self, state):
-        expand = False
-        this_node = self.current_node
-        valid_move = utils.valid_move(state)
-        while not expand:
-            state_prob, state_v = self.NN.eval(state)
-
-            for move in valid_move:
-                this_node.add_child(node(utils.generate_new_state()), move, state_prob[move])
+    def simulation(self):
+        for _ in range(self.s_per_step):
+            expand = False
+            this_node = self.current_node
+            self.simulate_game.simulate_reset(self.game_process.current_board_state())
+            state = self.simulate_game.current_board_state()
+            while not expand:
+                if this_node.eval_or_not():
+                    state_prob, state_v = self.NN.eval(state)
+                    valid_move = utils.valid_move(state)
+                    for move in valid_move:
+                        this_node.add_child(action=move, priorP=state_prob[move[0]*self.board_size + move[1]])
 
     def game(self):
 
